186b384f738bccc6d3ab9b9995fb0c76fdd85627
KanagawaMarcos <mcs1901@outlook.com>
2022-09-25 00:02:51 -0300

    feat: write the text read to a file on disk

--- Engine/Library.fs
+++ Engine/Library.fs
@@ -1,12 +1,15 @@
 ﻿namespace Engine
-
+open System.IO
 open Tesseract
 
 module Ocr =
     let score = 
         let path = __SOURCE_DIRECTORY__ + "/tessdata"
-        let file = __SOURCE_DIRECTORY__ + "/img/original2.jpg"
+        let file = __SOURCE_DIRECTORY__ + "/img/original.jpg"
+        let output = __SOURCE_DIRECTORY__ + "/artifacts/output.txt"
         let engine = new TesseractEngine(path, "eng", EngineMode.Default)
         let img = Pix.LoadFromFile(file)
         let page = engine.Process(img)
+        File.WriteAllText(output, page.GetText()) |> ignore
+
         (page.GetText(), page.GetMeanConfidence())    
