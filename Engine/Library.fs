39b349133d8296eee83f4b34d90747e66ddb4cde
KanagawaMarcos <mcs1901@outlook.com>
2022-09-24 23:37:10 -0300

    feat: extract text from image and print on console

--- Engine/Library.fs
+++ Engine/Library.fs
@@ -1,5 +1,12 @@
 ﻿namespace Engine
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open Tesseract
+
+module Ocr =
+    let score = 
+        let path = __SOURCE_DIRECTORY__ + "/tessdata"
+        let file = __SOURCE_DIRECTORY__ + "/img/original2.jpg"
+        let engine = new TesseractEngine(path, "eng", EngineMode.Default)
+        let img = Pix.LoadFromFile(file)
+        let page = engine.Process(img)
+        (page.GetText(), page.GetMeanConfidence())    
